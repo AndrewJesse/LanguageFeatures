69eebf85e0c90a1ab31160da64dcb9ca63003a95
A.J. Marchese <aj.marchese@outlook.com>
2023-03-04 22:22:10 -0800

    Added target typed new matching

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿//using Microsoft.AspNetCore.Mvc;
+//using LanguageFeatures.Models;
+
+namespace LanguageFeatures.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            Dictionary<string, Product> products = new()
+            {
+                ["Kayak"] = new Product { Name = "Kayak", Price = 275M },
+                ["Lifejacket"] = new Product { Name = "Lifejacket", Price = 48.95M }
+            };
+            return View("Index", products.Keys);
+        }
+    }
+}
