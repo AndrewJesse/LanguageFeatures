efbe84b75839331564b8ac5c1e931167b5a76e25
A.J. Marchese <aj.marchese@outlook.com>
2023-03-05 12:45:31 -0800

    pit stop

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -7,12 +7,21 @@ namespace LanguageFeatures.Controllers
     {
         public ViewResult Index()
         {
-            Dictionary<string, Product> products = new()
-            {
-                ["Kayak"] = new Product { Name = "Kayak", Price = 275M },
-                ["Lifejacket"] = new Product { Name = "Lifejacket", Price = 48.95M }
+            ShoppingCart cart = new ShoppingCart { Products = Product.GetProducts() };
+            
+            Product[] productArray = {
+                new Product {Name = "Kayak", Price = 275M},
+                new Product {Name = "Lifejacket", Price = 48.95M},
+                new Product {Name = "Soccer ball", Price = 19.50M},
+                new Product {Name = "Corner flag", Price = 34.95M}
             };
-            return View("Index", products.Keys);
+
+            decimal priceFilterTotal = productArray.FilterByPrice(20).TotalPrices();
+            decimal nameFilterTotal = productArray.FilterByName('S').TotalPrices();
+
+            return View("Index", new string[] { 
+                $"Price Total: {priceFilterTotal:C2}", 
+                $"Name Total: {nameFilterTotal:C2}" });
         }
     }
 }
